a42ce1a9e02b72a1bfa2e2658cb4d4235b497a18
Green Sky <green@g-s.xyz>
2024-10-24 00:40:18 +0200

    add subscription reference "counting" RAII wrapper

--- solanaceae/util/event_provider.hpp
+++ solanaceae/util/event_provider.hpp
@@ -8,11 +8,46 @@ template<typename EventI>
 struct EventProviderI {
 	using enumType = typename EventI::enumType;
 
+	// keeps track of subscriptions for you
+	// and destorys them on destruction
+	struct SubscriptionReference {
+		EventProviderI& _ep;
+		EventI* _object {nullptr};
+		std::vector<enumType> _subs;
+
+		SubscriptionReference(EventProviderI& ep, EventI* object) :
+			_ep(ep), _object(object)
+		{
+		}
+
+		~SubscriptionReference(void) {
+			for (const enumType et : _subs) {
+				auto& o_vec = _ep._subscribers.at(static_cast<size_t>(et));
+				for (auto o_it = o_vec.cbegin(); o_it != o_vec.cend(); o_it++) {
+					if (*o_it == _object) {
+						o_vec.erase(o_it);
+						break;
+					}
+				}
+			}
+		}
+
+		SubscriptionReference& subscribe(const enumType event_type) {
+			_ep._subscribers.at(static_cast<size_t>(event_type)).push_back(_object);
+			_subs.push_back(event_type);
+			return *this;
+		}
+	};
+
 	virtual ~EventProviderI(void) {};
 
 	// TODO: unsub
 	virtual void subscribe(EventI* object, const enumType event_type) {
 		_subscribers.at(static_cast<size_t>(event_type)).push_back(object);
+	}
+
+	SubscriptionReference newSubRef(EventI* object) {
+		return SubscriptionReference{*this, object};
 	}
 
 	protected:
